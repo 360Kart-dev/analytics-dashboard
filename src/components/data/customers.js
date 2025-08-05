076c8343375ddfe92b9e456570116d717db9a566
kwamebaah1 <baahhprince79@gmail.com>
2025-08-05 00:02:52 +0000

    Updates

--- src/components/data/customers.js
+++ src/components/data/customers.js
@@ -43,5 +43,320 @@ export const customers = [
     orders: 7,
     totalSpent: '¢1,120',
     lastOrder: '2023-12-12'
+  },
+  {
+    id: 6,
+    name: 'Kofi Ansah',
+    email: 'kofi.ansah@yahoo.com',
+    location: 'Tamale',
+    orders: 9,
+    totalSpent: '¢1,450',
+    lastOrder: '2023-12-05'
+  },
+  {
+    id: 7,
+    name: 'Esi Nyarko',
+    email: 'esi.nyarko@outlook.com',
+    location: 'Sunyani',
+    orders: 4,
+    totalSpent: '¢720',
+    lastOrder: '2023-11-30'
+  },
+  {
+    id: 8,
+    name: 'Kwame Agyeman',
+    email: 'agyeman.kwame@gmail.com',
+    location: 'Koforidua',
+    orders: 6,
+    totalSpent: '¢980',
+    lastOrder: '2023-12-08'
+  },
+  {
+    id: 9,
+    name: 'Ama Serwaa',
+    email: 'serwaa.ama@hotmail.com',
+    location: 'Obuasi',
+    orders: 11,
+    totalSpent: '¢2,150',
+    lastOrder: '2023-12-17'
+  },
+  {
+    id: 10,
+    name: 'Yaa Poku',
+    email: 'yaa.poku@gmail.com',
+    location: 'Sekondi',
+    orders: 2,
+    totalSpent: '¢380',
+    lastOrder: '2023-11-15'
+  },
+  {
+    id: 11,
+    name: 'Nii Laryea',
+    email: 'nii.laryea@yahoo.com',
+    location: 'Accra',
+    orders: 15,
+    totalSpent: '¢3,200',
+    lastOrder: '2023-12-20'
+  },
+  {
+    id: 12,
+    name: 'Afia Konadu',
+    email: 'afia.k@gmail.com',
+    location: 'Kumasi',
+    orders: 7,
+    totalSpent: '¢1,100',
+    lastOrder: '2023-12-14'
+  },
+  {
+    id: 13,
+    name: 'Kwesi Andoh',
+    email: 'k.andoh@outlook.com',
+    location: 'Tema',
+    orders: 5,
+    totalSpent: '¢850',
+    lastOrder: '2023-12-09'
+  },
+  {
+    id: 14,
+    name: 'Mansa Aboagye',
+    email: 'mansa.aboagye@gmail.com',
+    location: 'Cape Coast',
+    orders: 8,
+    totalSpent: '¢1,300',
+    lastOrder: '2023-12-16'
+  },
+  {
+    id: 15,
+    name: 'Kojo Frempong',
+    email: 'k.frempong@yahoo.com',
+    location: 'Takoradi',
+    orders: 4,
+    totalSpent: '¢670',
+    lastOrder: '2023-11-25'
+  },
+  {
+    id: 16,
+    name: 'Akua Dufie',
+    email: 'akua.dufie@gmail.com',
+    location: 'Kumasi',
+    orders: 10,
+    totalSpent: '¢1,800',
+    lastOrder: '2023-12-19'
+  },
+  {
+    id: 17,
+    name: 'Osei Kuffour',
+    email: 'osei.k@hotmail.com',
+    location: 'Accra',
+    orders: 6,
+    totalSpent: '¢950',
+    lastOrder: '2023-12-07'
+  },
+  {
+    id: 18,
+    name: 'Abena Serwaa',
+    email: 'abena.s@gmail.com',
+    location: 'Tamale',
+    orders: 3,
+    totalSpent: '¢520',
+    lastOrder: '2023-11-20'
+  },
+  {
+    id: 19,
+    name: 'Kweku Ananse',
+    email: 'k.ananse@outlook.com',
+    location: 'Sunyani',
+    orders: 7,
+    totalSpent: '¢1,150',
+    lastOrder: '2023-12-11'
+  },
+  {
+    id: 20,
+    name: 'Esi Bonsu',
+    email: 'esi.bonsu@yahoo.com',
+    location: 'Koforidua',
+    orders: 5,
+    totalSpent: '¢890',
+    lastOrder: '2023-12-04'
+  },
+  {
+    id: 21,
+    name: 'Kwaku Manu',
+    email: 'k.manu@gmail.com',
+    location: 'Obuasi',
+    orders: 9,
+    totalSpent: '¢1,420',
+    lastOrder: '2023-12-18'
+  },
+  {
+    id: 22,
+    name: 'Ama Ampofo',
+    email: 'ama.ampofo@hotmail.com',
+    location: 'Sekondi',
+    orders: 4,
+    totalSpent: '¢730',
+    lastOrder: '2023-11-29'
+  },
+  {
+    id: 23,
+    name: 'Yaw Sarpong',
+    email: 'yaw.sarpong@gmail.com',
+    location: 'Accra',
+    orders: 12,
+    totalSpent: '¢2,400',
+    lastOrder: '2023-12-21'
+  },
+  {
+    id: 24,
+    name: 'Adjoa Darko',
+    email: 'adjoa.darko@yahoo.com',
+    location: 'Kumasi',
+    orders: 6,
+    totalSpent: '¢1,020',
+    lastOrder: '2023-12-13'
+  },
+  {
+    id: 25,
+    name: 'Nii Ayi',
+    email: 'nii.ayi@outlook.com',
+    location: 'Tema',
+    orders: 8,
+    totalSpent: '¢1,350',
+    lastOrder: '2023-12-17'
+  },
+  {
+    id: 26,
+    name: 'Efua Badu',
+    email: 'efua.badu@gmail.com',
+    location: 'Cape Coast',
+    orders: 5,
+    totalSpent: '¢880',
+    lastOrder: '2023-12-06'
+  },
+  {
+    id: 27,
+    name: 'Kofi Gyamfi',
+    email: 'k.gyamfi@yahoo.com',
+    location: 'Takoradi',
+    orders: 7,
+    totalSpent: '¢1,120',
+    lastOrder: '2023-12-14'
+  },
+  {
+    id: 28,
+    name: 'Ama Nyamekye',
+    email: 'ama.nyamekye@hotmail.com',
+    location: 'Kumasi',
+    orders: 10,
+    totalSpent: '¢1,750',
+    lastOrder: '2023-12-19'
+  },
+  {
+    id: 29,
+    name: 'Kwame Asante',
+    email: 'kwame.asante@gmail.com',
+    location: 'Accra',
+    orders: 14,
+    totalSpent: '¢2,800',
+    lastOrder: '2023-12-22'
+  },
+  {
+    id: 30,
+    name: 'Akosua Frimpong',
+    email: 'akosua.f@outlook.com',
+    location: 'Tamale',
+    orders: 3,
+    totalSpent: '¢510',
+    lastOrder: '2023-11-22'
+  },
+  {
+    id: 31,
+    name: 'Yaw Ofori',
+    email: 'yaw.ofori@yahoo.com',
+    location: 'Sunyani',
+    orders: 6,
+    totalSpent: '¢990',
+    lastOrder: '2023-12-09'
+  },
+  {
+    id: 32,
+    name: 'Abena Kumi',
+    email: 'abena.kumi@gmail.com',
+    location: 'Koforidua',
+    orders: 5,
+    totalSpent: '¢870',
+    lastOrder: '2023-12-05'
+  },
+  {
+    id: 33,
+    name: 'Kweku Appiah',
+    email: 'k.appiah@hotmail.com',
+    location: 'Obuasi',
+    orders: 8,
+    totalSpent: '¢1,300',
+    lastOrder: '2023-12-16'
+  },
+  {
+    id: 34,
+    name: 'Esi Owusu',
+    email: 'esi.owusu@yahoo.com',
+    location: 'Sekondi',
+    orders: 4,
+    totalSpent: '¢710',
+    lastOrder: '2023-11-27'
+  },
+  {
+    id: 35,
+    name: 'Kwasi Boakye',
+    email: 'kwasi.boakye@gmail.com',
+    location: 'Accra',
+    orders: 11,
+    totalSpent: '¢2,050',
+    lastOrder: '2023-12-20'
+  },
+  {
+    id: 36,
+    name: 'Ama Asare',
+    email: 'ama.asare@outlook.com',
+    location: 'Kumasi',
+    orders: 7,
+    totalSpent: '¢1,180',
+    lastOrder: '2023-12-15'
+  },
+  {
+    id: 37,
+    name: 'Yaw Mensah',
+    email: 'yaw.mensah@yahoo.com',
+    location: 'Tema',
+    orders: 9,
+    totalSpent: '¢1,460',
+    lastOrder: '2023-12-18'
+  },
+  {
+    id: 38,
+    name: 'Adwoa Twum',
+    email: 'adwoa.twum@gmail.com',
+    location: 'Cape Coast',
+    orders: 6,
+    totalSpent: '¢1,030',
+    lastOrder: '2023-12-11'
+  },
+  {
+    id: 39,
+    name: 'Kofi Adjei',
+    email: 'kofi.adjei@hotmail.com',
+    location: 'Takoradi',
+    orders: 5,
+    totalSpent: '¢840',
+    lastOrder: '2023-12-04'
+  },
+  {
+    id: 40,
+    name: 'Akua Agyeman',
+    email: 'akua.agyeman738@yahoo.com',
+    location: 'Kumasi',
+    orders: 12,
+    totalSpent: '¢2,350',
+    lastOrder: '2023-12-21'
   }
 ];
